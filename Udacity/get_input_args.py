b57a97eea74c60e2e35569aa3a52b856c6c12880
nrajaman <49346714+nrajaman@users.noreply.github.com>
2019-04-06 09:55:30 -0700

    Add files via upload

---
+++ Udacity/get_input_args.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# */AIPND-revision/intropyproject-classify-pet-images/get_input_args.py
+#                                                                             
+# PROGRAMMER: 
+# DATE CREATED:                                   
+# REVISED DATE: 
+# PURPOSE: Create a function that retrieves the following 3 command line inputs 
+#          from the user using the Argparse Python module. If the user fails to 
+#          provide some or all of the 3 inputs, then the default values are
+#          used for the missing inputs. Command Line Arguments:
+#     1. Image Folder as --dir with default value 'pet_images'
+#     2. CNN Model Architecture as --arch with default value 'vgg'
+#     3. Text File with Dog Names as --dogfile with default value 'dognames.txt'
+#
+##
+# Imports python modules
+import argparse
+
+# TODO 1: Define get_input_args function below please be certain to replace None
+#       in the return statement with parser.parse_args() parsed argument 
+#       collection that you created with this function
+# 
+def get_input_args():
+    parser = argparse.ArgumentParser(description='Process Command Line Input')
+    parser.add_argument('--dir', type=str, default='pet_images/', help='Image Folder')
+    parser.add_argument('--arch', type=str, default='vgg',help='CNN Model Architecture')
+    parser.add_argument('--dogfile', type=str,default='dognames.txt', help='Dog Names')
+    return parser.parse_args()
+
+   
